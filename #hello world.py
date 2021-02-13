cc0d323c898ebfc33402e71579bf74988afe4a76
suwayne <osasu.ogbebor@gmail.com>
2021-02-14 00:00:57 +0100

    new commit

---
+++ #hello world.py
@@ -0,0 +1,9 @@
+#hello world
+ask_name = input("what is your name?:")
+
+print('you are welcome ' + ask_name)
+print('the length of your name is :' + str(len(ask_name)))
+
+#ask for age
+store_age = input('what is your age?: ')
+print('you will be, ' + str(int(store_age) + 1) + ' a year from now')
