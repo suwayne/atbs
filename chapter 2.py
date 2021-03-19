1273c8ea1a454c58464e9e66b7be292a6e992cd3
suwayne <osasu.ogbebor@gmail.com>
2021-03-19 19:35:22 +0100

    new commit

--- chapter 2.py
+++ chapter 2.py
@@ -11,7 +11,6 @@
 #     spam = spam + 1
 
 # name = ''
-
 # while name != 'your name':
 #     print('please type your name.')
 #     name = input()
@@ -25,16 +24,16 @@
 
 # print('Thank you!')
 
-# while True:
-#     print('Who are you?')
-#     name = input()
-#     if name != 'Joe':
-#         continue
-#     print('Hello, Joe. What is the password? (It is a fish.)')
-#     password = input()
-#     if password == 'swordfish':
-#         break
-# print('Access granted.')
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('Hello, Joe. What is the password? (It is a fish.)')
+    password = input()
+    if password == 'swordfish':
+        break
+print('Access granted.')
 
 """TRUTHY AND FALSEY VALUES"""
 
@@ -115,3 +114,7 @@ while True:
     print('You typed ' + response + '.')
 
 
+name = 'Osasu'
+
+if name == 'Osasu':
+  print('Hello, Osasu')
---
+++ chapter3.py
@@ -0,0 +1,16 @@
+"""creating a function"""
+# def hello():
+#     print('howdy!')
+#     print('today is going to be a good day')
+#     print('always learn the basics')
+
+# hello()
+
+"""hello function 2"""
+
+def hello(name):
+    print('Hello ' + name)
+
+hello('suwayne')
+
+
